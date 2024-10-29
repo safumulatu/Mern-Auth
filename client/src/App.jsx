a5865f59833971c6b6b4c5a7c5bb68659eb55592
safumulatu <safumulatu9@gmail.com>
2024-10-29 10:05:08 +0300

    create navbar components and link them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-    </div>
+    <>
+      {/* {//header} */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 };
 export default App;
